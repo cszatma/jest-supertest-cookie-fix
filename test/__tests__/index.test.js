e36a61b831dbf3fcf5cb5c29e7b7fc505b4e0f80
cszatma <christopherszatmary@gmail.com>
2018-04-12 15:32:11 -0400

    Added tests for the fixedSupertestAgent function

--- test/__tests__/index.test.js
+++ test/__tests__/index.test.js
@@ -3,31 +3,50 @@ const fixedAgent = require('../../src');
 
 const app = require('../app');
 
+const fixedSupertestAgent = fixedAgent.fixedSupertestAgent;
+
+const mockCookies = {
+    'test-cookie-1':
+        'session=eyJwYXNzcG9ydCI6eyJ1c2VyIjoiMTIzNCJ9fQ==; 12 May 2018 12:35:31 GMT',
+    'test-cookie-2':
+        'session.sig=HBY87i6rkzCPNIZ46ZWW1bz3bno; 12 May 2018 12:35:31 GMT',
+};
+
 describe('Test the regular agent', () => {
     const agent = request.agent(app);
-    const mockCookies = {
-        'test-cookie-1':
-            'session=eyJwYXNzcG9ydCI6eyJ1c2VyIjoiMTIzNCJ9fQ==; 12 May 2018 12:35:31 GMT',
-    };
+    const mockCookie = { 'test-cookie-1': mockCookies['test-cookie-1'] };
 
     it('should set the cookies', () => {
         return agent.get('/set-cookie').expect(200, { status: 'ok' });
     });
 
     it('should only return the first cookie with the normal agent', () => {
-        return agent.get('/get-cookie').expect(200, mockCookies);
+        return agent.get('/get-cookie').expect(200, mockCookie);
     });
 });
 
 describe('Test the fixed agent', () => {
-    // const agent = request.agent(app);
     const agent = fixedAgent(app);
-    const mockCookies = {
-        'test-cookie-1':
-            'session=eyJwYXNzcG9ydCI6eyJ1c2VyIjoiMTIzNCJ9fQ==; 12 May 2018 12:35:31 GMT',
-        'test-cookie-2':
-            'session.sig=HBY87i6rkzCPNIZ46ZWW1bz3bno; 12 May 2018 12:35:31 GMT',
-    };
+
+    it('should ', () => {
+        return agent.get('/set-cookie').expect(200, { status: 'ok' });
+    });
+
+    it('should ', () => {
+        return agent.get('/get-cookie').expect(200, mockCookies);
+    });
+});
+
+describe('Test the fixedSupertestAgent function', () => {
+    const supertestAgent = request.agent(app);
+    const agent = fixedSupertestAgent(supertestAgent);
+
+    it('should throw a TypeError', () => {
+        function fixAgent() {
+            fixedSupertestAgent(true);
+        }
+        expect(fixAgent).toThrowError(TypeError);
+    });
 
     it('should ', () => {
         return agent.get('/set-cookie').expect(200, { status: 'ok' });
